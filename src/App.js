eb6ad141174a9cf279080dcc800163bb840de0f3
mamun-prodhan <mamunpr9@gmail.com>
2023-07-21 02:51:31 +0600

    search bar added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
 import React from "react";
-import menuHome from '../src/assets/home-menu.svg';
-import contractors from '../src/assets/engineer.png';
+import menuHome from "../src/assets/home-menu.svg";
+import contractors from "../src/assets/engineer.png";
+import { SearchIcon } from "@heroicons/react/outline";
+import findTrade from '../src/assets/findatrade-01 4.png';
+import settings from '../src/assets/setting 2.png';
 
 function App() {
   return (
-    <div className="lg:flex">
+    <div className="lg:flex py-16 bg-[#F3F7F2;]">
       {/* menu div */}
-      <div className="w-[100%] lg:w-[20%] flex lg:block items-center justify-around py-3 bg-red-300">
+      <div className="w-[100%] lg:w-[20%] flex lg:block items-center justify-around py-3 bg-[#F3F7F2]">
         <div className="flex flex-col items-center">
-          <img className="w-[56px] h-[49px] hover:bg-[#3F8825] rounded-xl p-3  hover:bg-opacity-[31%]" src={menuHome} alt="dashboard" />
+          <img
+            className="w-[56px] h-[49px] hover:bg-[#3F8825] rounded-xl p-3  hover:bg-opacity-[31%]"
+            src={menuHome}
+            alt="dashboard"
+          />
           <p className="text-xs font-semibold mt-1">Dashboard</p>
         </div>
         <div className="flex flex-col items-center  lg:my-16">
-          <img className="w-[56px] h-[49px] hover:bg-[#3F8825] rounded-xl p-3  hover:bg-opacity-[31%]" src={menuHome} alt="contractors" />
+          <img
+            className="w-[56px] h-[49px] hover:bg-[#3F8825] rounded-xl p-3  hover:bg-opacity-[31%]"
+            src={menuHome}
+            alt="contractors"
+          />
           <p className="text-xs font-semibold mt-1">Contractors</p>
         </div>
         <div className="flex flex-col items-center">
-          <img className="w-[56px] h-[49px] hover:bg-[#3F8825] rounded-xl p-3  hover:bg-opacity-[31%]" src={menuHome} alt="users" />
+          <img
+            className="w-[56px] h-[49px] hover:bg-[#3F8825] rounded-xl p-3  hover:bg-opacity-[31%]"
+            src={menuHome}
+            alt="users"
+          />
           <p className="text-xs font-semibold mt-1">Users</p>
-        </div> 
+        </div>
       </div>
       {/* graph div */}
-      <div className="w-[80%] h-[200px] bg-red-700">
-
+      <div className="w-[100%] lg:w-[80%] h-[200px] bg-red-200 mt-5">
+        {/* search bar */}
+        <div className="">
+          <form action="" className="w-full m-0 md:mx-auto lg:m-0 md:max-w-[80%] lg:max-w-[60%]">
+            <div className="relative flex items-center">
+              <SearchIcon className="w-5 h-6 text-[#081D03] absolute ml-3 pointer-events-none"></SearchIcon>
+              <input
+                type="text"
+                name="search"
+                placeholder="Search"
+                autoComplete="off"
+                className="pr-3 pl-10 py-4 w-full font-semibold text-[#626262] text-xs rounded-xl border-none focus:outline-[#3F8825] focus:ring-2"
+              />
+            </div>
+          </form>
+          {/* <button className="flex items-center p-4 bg-[#FFF] hover:bg-[#3F8825] rounded-full">
+            <img className="me-4" src={findTrade} alt="logo" />
+            <img src={settings} alt="settings" />
+          </button> */}
+        </div>
       </div>
     </div>
   );
