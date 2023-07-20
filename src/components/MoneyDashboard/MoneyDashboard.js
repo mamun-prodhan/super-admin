18bc0b97c4d89f125d405f582c272bd8ba264a79
mamun-prodhan <mamunpr9@gmail.com>
2023-07-20 19:21:57 +0600

    responsive completed

--- src/components/MoneyDashboard/MoneyDashboard.js
+++ src/components/MoneyDashboard/MoneyDashboard.js
@@ -6,13 +6,14 @@ import arrow3 from "../../assets/arrow3.png";
 import cross from "../../assets/delete.png";
 import error from "../../assets/error.png";
 import export1 from "../../assets/export.png";
-import goal from '../../assets/goal.png';
+import goal from "../../assets/goal.png";
+import home from '../../assets/home.png';
 
 const MoneyDashboard = () => {
   return (
-    <div className="p-12">
-      <div className=" border-2 border-sky-500 rounded-xl p-12">
-        <div className="w-[30%]">
+    <div className="lg:p-12">
+      <div className=" border-2 lg:flex gap-10 border-sky-500 rounded-xl p-5 lg:p-12">
+        <div className="w-[100%] md:w-[100%] lg:w-[30%]">
           <p className="text-xl font-semibold">Processing</p>
           <h3 className="text-3xl font-bold">$0.00 USD</h3>
           {/* money details */}
@@ -97,18 +98,88 @@ const MoneyDashboard = () => {
                   src={goal}
                   alt=""
                 />
-                <p className="mx-3 font-semibold text-[#787777]">Set an earnings goal</p>
+                <p className="mx-3 font-semibold text-[#787777]">
+                  Set an earnings goal
+                </p>
               </div>
               <p className="mx-3 font-semibold text-[#928CF9]">Create goal</p>
             </div>
-            <hr  className="my-3"/>
+            <hr className="my-3" />
             <div>
-                <p className="mx-3 font-semibold text-[#787777]">Earn my first $10</p>
+              <p className="mx-3 font-semibold text-[#787777]">
+                Earn my first $10
+              </p>
             </div>
           </div>
         </div>
         {/* graph */}
-        <div className="w-[70%]"></div>
+        <div className="w-[100%] md:w-[100%] lg:w-[70%]">
+          {/* graph div */}
+          <div className="h-[300px]"></div>
+          {/* transaction div */}
+          <div>
+            <div className="md:flex lg:flex items-center justify-between">
+              <div>
+                <h3 className="text-3xl text-center md:text-start lg:text-start  font-bold">Transactions</h3>
+              </div>
+              <div className="md:flex lg:flex items-center">
+                <div className="flex items-center justify-center my-3 md:my-0 lg:my-0 mr-3 py-2 px-4 rounded-3xl bg-[#EAEAEA]">
+                  <img src={export1} alt="export" />
+                  <p className="ms-2 font-semibold">Export</p>
+                </div>
+                <div className="flex items-center justify-center mb-3 md:mb-0 lg:mb-0 mr-3 py-2 px-4 rounded-3xl bg-[#EAEAEA]">
+                  <p className="me-2 font-semibold">All activity</p>
+                  <img src={arrow1} alt="export" />
+                </div>
+                <div className="flex items-center justify-center mb-3 md:mb-0 lg:mb-0 py-2 px-4 rounded-3xl bg-[#EAEAEA]">
+                  <p className="me-2 font-semibold">All time</p>
+                  <img src={arrow1} alt="export" />
+                </div>
+              </div>
+            </div>
+            <div className="my-5">
+              <table className="w-[100%] font-semibold text-[#787777]">
+                <tr className="text-left text-xs md:text-sm lg:text-sm">
+                  <th>TYPE</th>
+                  <th>DATE</th>
+                  <th>AMOUNT</th>
+                </tr>
+                <tr className="border-b ">
+                  <td className="flex items-center py-4">
+                    <img className="me-2 md:me-5 lg:me-5 p-1 bg-[#EAEAEA] rounded-full" src={money} alt="" />
+                    <span>Payout from Demo to STRIPE TEST BANK 6789</span>
+                  </td>
+                  <td className="py-4">Apr 11</td>
+                  <td className="py-4"><span>$18.22</span> <span className="md:ms-5 lg:ms-5 rounded-md px-1 py-0.5 text-sm bg-[#D3F3CD]">Settled</span></td>
+                </tr>
+                <tr className="border-b ">
+                  <td className="flex items-center py-4">
+                    <img className="me-2 md:me-5 lg:me-5 p-1 bg-[#EAEAEA] rounded-full" src={money} alt="" />
+                    <span>Guitter Lesson</span>
+                  </td>
+                  <td className="py-4">Apr 11</td>
+                  <td className="py-4"><span>$38.22</span> <span className="md:ms-5 lg:ms-5 rounded-md px-1 py-0.5 text-sm bg-[#D3F3CD]">Settled</span></td>
+                </tr>
+                <tr className="border-b ">
+                  <td className="flex items-center py-4">
+                    <img className="me-2 md:me-5 lg:me-5 p-1 bg-[#EAEAEA] rounded-full" src={money} alt="" />
+                    <span>Payout from Demo to STRIPE TEST BANK 6789</span>
+                  </td>
+                  <td className="py-4">Apr 11</td>
+                  <td className="py-4"><span>$20.22</span> <span className="md:ms-5 lg:ms-5 rounded-md px-1 py-0.5 text-sm bg-[#D3F3CD]">Settled</span></td>
+                </tr>
+                <tr className="border-b ">
+                  <td className="flex items-center py-4">
+                    <img className="me-2 md:me-5 lg:me-5 p-1 bg-[#EAEAEA] rounded-full" src={money} alt="" />
+                    <span>Guitter Lesson</span>
+                  </td>
+                  <td className="py-4">Apr 11</td>
+                  <td className="py-4"><span>$10.22</span> <span className="md:ms-5 lg:ms-5 rounded-md px-1 py-0.5 text-sm bg-[#D3F3CD]">Settled</span></td>
+                </tr>
+              </table>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
